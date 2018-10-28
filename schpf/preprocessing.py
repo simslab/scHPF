7ad60b9a32af53140ae60e6799eeb5501868b0d4
Hanna Mendes Levitin <h.mendes.levitin@gmail.com>
2018-10-28 14:34:56 -0400

    cli

---
+++ schpf/preprocessing.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import numpy as np
+from scipy.sparse import coo_matrix
+
+# import pandas as pd
+
+
+def load_tab_delim(filename):
+    pass
+
+
+def load_loom(filename):
+    pass
+
+
+def load_coo(filename):
+    raw = np.loadtxt(filename, delimiter='\t', dtype=int)
+    sparse = coo_matrix((raw[:,2], (raw[:,0],raw[:,1])))
+    return sparse
+
+
--- setup.py
+++ setup.py
@@ -1,17 +1,20 @@
-import setuptools
+#!/usr/bin/env python
 
-setuptools.setup(
+from setuptools import find_packages, setup
+
+__version__ = '0.2.0'
+
+setup(
     name='scHPF',
+    version=__version__,
+    packages=find_packages(),
+    scripts=['bin/scHPF'],
+    python_requires='>=3.6',
+    install_requires= ['numpy', 'numba', 'scikit-learn'],
+    extras_require={},
+    author = 'Hanna Mendes Levitin',
+    author_email = 'hml2134@columbia.edu',
     description='Single-cell Hierarchical Poisson Factorization',
-    version='0.2',
-    url='https://www.github.com/simslab/scHPF',
     license='GPLv3',
-    install_requires=[
-        'numpy',
-        'scikit-learn',
-        'numba'
-    ],
-    extras_require={
-    },
-    packages=setuptools.find_packages(),
+    url='https://www.github.com/simslab/scHPF',
 )
